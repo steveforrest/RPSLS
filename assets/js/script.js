c53b982b8dfdca3f8dbab84dfcf1092562f42da9
steveforrest <steveforrest_1@hotmail.com>
2021-06-23 11:25:47 +0000

    added logic so computer can generate its choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
-    for(let button of buttons){
-        button.addEventListener("click", function(){
-            if(this.getAttribute("data-type")=== "submit"){
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
                 alert(" you clicked submit");
-            }else{
+            } else {
                 let userChoice = this.getAttribute("data-type");
                 alert(`you clicked ${userChoice}`);
             }
@@ -13,26 +13,54 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 })
 
-function runGame(){
+function runGame() {
 
 }
 
-function takePlayerChoice(){
+/**Users choice is taken */
+
+function takePlayerChoice() {
+
 
 }
 
-function generateComputerChoice(){
+takePlayerChoice()
+
+/**
+ * Generates a number between 1 and 5 and asigns it to a computer choice for the game
+ * @returns 
+ */
+
+
+function generateComputerChoice() {
+
+    let comChoice = Math.ceil(Math.random() * 5);
+
+    if (comChoice === 1) {
+        return "Rock";
+    } else if (comChoice === 2) {
+        return "Paper";
+    } else if (comChoice === 3) {
+        return "Scissors";
+    } else if (comChoice === 4){
+        return "Lizard";
+    } else if (comChoice === 5){
+    return "Spock";
+}else{
+    return "error";
+}
+}
+
+console.log(generateComputerChoice())
+
+function decideWinner() {
 
 }
 
-function decideWinner(){
+function incrementScore() {
 
 }
 
-function incrementScore(){
-    
-}
-
-function incrementLoss(){
+function incrementLoss() {
 
 }
