abe9bd2da743ea1617037e136b1920665039252a
steveforrest <steveforrest_1@hotmail.com>
2021-06-29 09:31:11 +0000

    added css for image rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,7 +87,7 @@ function incrementLoss() {
  */
 
 function win() {
-    let win2Win = parseInt(document.getElementsByTagName("label"))
+    let win2Win = parseInt(document.getElementsByClassName("howManyWins"))
     console.log(win2Win)
 }
 win()
@@ -96,3 +96,16 @@ win()
 /**
  * to allow to hide and show sections
  */
+
+function showRules(){
+    let showImg = document.getElementById("rulesImg");
+    if(showImg.style.display == "none"){
+        showImg.style.display == "block";
+    }else{
+        showImg.style.display == "none";
+    }
+    
+    console.log(showImg);
+}
+
+showRules()
