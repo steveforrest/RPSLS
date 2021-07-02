880bf495ae40b392c13df8f942111e63585777fd
steveforrest <steveforrest_1@hotmail.com>
2021-07-02 11:30:14 +0000

    added call showResult function to decide winner function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,4 @@
 
-// get close button
-var closeModal = document.getElementsByClassName('close-button')[0];
-
-let startingStateModal = getComputedStyle(closeModal).getPropertyValue('display');
-//listen for click
-closeModal.addEventListener('click', closeModal);
-console.log(closeModal)
 /**
  * Generates a number between 1 and 5 and asigns it to a computer choice for the game
  * @returns 
@@ -43,25 +36,32 @@ function decideWinner(userChoice) {
 
     if (userChoice === comChoice) {
         console.log("game drew");
+        showResult();
     } else {
         if (userChoice === "Rock" && (comChoice == "Scissors" || comChoice == "Lizard")) {
             console.log("you win")
             incrementScore();
+            showResult();
         } else if (userChoice === "Paper" && (comChoice == "Rock" || comChoice == "Spock")) {
             console.log("you win")
             incrementScore();
+            showResult();
         } else if (userChoice === "Scissors" && (comChoice == "Paper" || comChoice == "Lizard")) {
             console.log("you win")
             incrementScore();
+            showResult();
         } else if (userChoice === "Lizard" && (comChoice == "Spock" || comChoice == "Paper")) {
             console.log("you win")
             incrementScore();
+            showResult();
         } else if (userChoice === "Spock" && (comChoice == "Scissors" || Comment == "Rock")) {
             console.log("you win")
             incrementScore();
+            showResult();
         } else {
             console.log("you lose")
             incrementLoss();
+            showResult();
         }
     }
 
@@ -120,61 +120,42 @@ function showRules() {
     let startingStateModal = getComputedStyle(modal).getPropertyValue('display');
 
     modal.style.display = "flex";
+}
 
 /**
  * to allow to hide rules
  */
 
-
-}
-
-/*function hideRules() {
+function hideRules() {
     //get modal element
     let modal = document.getElementById("rules-modal");
     //get open modal button
-    // let RulesButton = document.getElementsByID("modal-button");
+     let RulesButton = document.getElementsByID("modal-button");
     //get close modal button
     let closeModal = document.getElementById("closeModal");
     // get starting state of modal
     let startingStateModal = getComputedStyle(modal).getPropertyValue('display');
 
     modal.style.display = "none";
-}*/
-
-function closeModal(){
-    modal.style.display = 'none';
 }
 
-
-/**
- * If play game is clicked hero images is hidden and the game starts
- * scores are shown as well as buttons and selector for how many games to be played
- */
-
-
-function playGame() {
-    let hero = document.getElementById("hero-img");
-    let numberOfWins = document.getElementById("chooseWins");
-    let gameButtons = document.getElementById("container2");
-    let scoreBoard = document.getElementById("scores");
-
-    let startingStateHero = getComputedStyle(hero).getPropertyValue('display');
-
-    if (startingStateHero == "flex") {
-        // change the element style
-        hero.style.display = "none";
-        numberOfWins.style.display = "grid";
-        gameButtons.style.display = "grid";
-        scoreBoard.style.display = "flex";
-
-
-    } else {
-        console.log("already playing")
-    }
-
-}
 
 /**
  * Create a modal that is called up when the 
  * decide winner function is called and states what you chose what computer chose and the game result
  */
+
+ function showResult() {
+
+
+    //get modal element
+    let modal = document.getElementById("resultsModal");
+  
+    // get starting state of modal
+    let startingStateModal = getComputedStyle(modal).getPropertyValue('display');
+
+    modal.style.display = "block";
+
+
+
+}
